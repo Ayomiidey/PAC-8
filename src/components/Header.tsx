1f0f99f0c4fc40c4f5bb792bdddf447211cb26c2
Ayomiidey <quadriayomiidey@gmail.com>
2025-02-12 15:23:27 +0100

    fixed header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -54,57 +54,64 @@ import pac8Logo from "../assets/images/pac8Logo.png";
 
 const Header = () => {
   return (
-    <div>
-      <div className="w-full h-12 text-center text-xs bg-purple-400 text-white flex flex-col justify-center items-center font-medium">
-        <p>Place your order now, and we’ll ship it out today!</p>
-        <p>Fast, tracked, and express delivery options available.</p>
+    <>
+      <div className="fixed top-0 left-0 right-0 z-50">
+        <div className="w-full h-12 text-center text-xs bg-purple-400 text-white flex flex-col justify-center items-center font-medium">
+          <p>Place your order now, and we'll ship it out today!</p>
+          <p>Fast, tracked, and express delivery options available.</p>
+        </div>
+
+        <nav className="bg-white border-b">
+          <div className="mx-auto px-4 md:px-16 lg:px-24 py-4 flex justify-between items-center">
+            <Link to="/">
+              <div className="text-lg font-bold">
+                <img src={pac8Logo} alt="Logo" className="h-[70px] w-auto" />
+              </div>
+            </Link>
+
+            <div className="relative flex-1 mx-4">
+              <form>
+                <input
+                  type="text"
+                  placeholder="Search Product"
+                  className="w-full border py-2 px-4"
+                />
+                <FaSearch className="absolute top-3 right-3 text-red-500" />
+              </form>
+            </div>
+            <div className="flex items-center space-x-4">
+              <Link to="/cart">
+                <FaShoppingCart className="text-lg" />
+              </Link>
+              <button className="hidden md:block">Login | Register</button>
+              <button className="block md:hidden">
+                <FaUser />
+              </button>
+            </div>
+          </div>
+
+          <div className="flex items-center justify-center space-x-10 py-4 text-sm font-bold">
+            <Link to="/" className="hover:underline">
+              Home
+            </Link>
+            <Link to="/products" className="hover:underline">
+              Shop
+            </Link>
+            <Link to="/about" className="hover:underline">
+              About Us
+            </Link>
+            <Link to="/contactUs" className="hover:underline">
+              Contact Us
+            </Link>
+          </div>
+        </nav>
       </div>
 
-      <nav className="bg-white">
-        <div className="mx-auto px-4 md:px-16 lg:px-24 py-4 flex justify-between items-center">
-          <Link to="/">
-            <div className="text-lg font-bold">
-              <img src={pac8Logo} alt="Logo" className="h-[70px] w-auto" />
-            </div>
-          </Link>
-
-          <div className="relative flex-1 mx-4">
-            <form>
-              <input
-                type="text"
-                placeholder="Search Product"
-                className="w-full border py-2 px-4"
-              />
-              <FaSearch className="absolute top-3 right-3 text-red-500"></FaSearch>
-            </form>
-          </div>
-          <div className="flex items-center space-x-4">
-            <Link to="/cart">
-              <FaShoppingCart className="text-lg" />
-            </Link>
-            <button className="hidden md:block">Login | Register</button>
-            <button className="block md:hidden">
-              <FaUser />
-            </button>
-          </div>
-        </div>
-
-        <div className="flex items-center justify-center space-x-10 py-4 text-sm font-bold">
-          <Link to="/" className="hover:underline">
-            Home
-          </Link>
-          <Link to="/products" className="hover:underline">
-            Shop
-          </Link>
-          <Link to="/about" className="hover:underline">
-            About Us
-          </Link>
-          <Link to="/contactUs" className="hover:underline">
-            Contact Us
-          </Link>
-        </div>
-      </nav>
-    </div>
+      <div className="pt-[168px]">
+        {/* I had this div there so my content don't go inside my fixed header */}
+      </div>
+    </>
   );
 };
+
 export default Header;
